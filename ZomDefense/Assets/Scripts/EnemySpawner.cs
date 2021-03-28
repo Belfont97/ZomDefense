2f610c6406ed397fa81557f4f97309f8fba592d4
Belfont97 <kbehren1@uncc.edu>
2021-03-28 13:09:13 -0400

    Update
    Working on GameManager, updated to newest engine

---
+++ ZomDefense/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyToSpawn;
+    public Transform spawnPosition;
+
+    public void spawnEnemy()
+    {
+        Instantiate(enemyToSpawn, spawnPosition.position, Quaternion.identity, spawnPosition);
+    }
+}
---
+++ ZomDefense/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int nightNumber = 1;
+    public int dayNumber = 1;
+
+    private GameObject player;
+    private GameObject enemySpawner;
+    private GameObject canvas;
+    private GameObject barricade;
+
+    private int enemiesToSpawn;
+    private int enemiesKilled;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        enemySpawner = GameObject.FindGameObjectWithTag("Enemy Spawner");
+
+        canvas = GameObject.FindGameObjectWithTag("UI");
+
+        barricade = GameObject.FindGameObjectWithTag("Barricade");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (State)
+        {
+            case State.SPAWNENEMIES:
+                enemiesToSpawn = 10;
+
+                for (int i = 0; i < enemiesToSpawn; i++)
+                    enemySpawner.GetComponent<EnemySpawner>().spawnEnemy();
+
+                if (enemiesKilled == 10)
+                    canvas.GetComponent<uiScript>().gameOver();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public enum State()
+    {
+        DEFAULT,
+        SPAWNENEMIES,
+        DAY,
+        NIGHT,
+        DESTROYBARRICADE,
+        GAMELOSE,
+        GAMEWIN,
+        DAYOVER,
+        NIGHTOVER
+    };
+}
